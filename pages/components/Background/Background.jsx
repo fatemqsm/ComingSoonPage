28a27f9d96864523dc58bbf1ff1d94c526517089
fatemqsm <ghasemlofatemeh@gmail.com>
2022-05-25 18:21:28 +0430

    commit on "transfer components folder to pages"

--- pages/components/Background/Background.jsx
+++ pages/components/Background/Background.jsx
@@ -29,7 +29,7 @@ export default function Background() {
           alt="background image"
           loading="lazy"
           layout="fill"
-          src="../../public/images/bg01.jpg"
+          src="/images/bg01.jpg"
           objectFit="cover"
           objectPosition="30% 30%"
         />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react'
 import Head from 'next/head'
-import Background from '../components/Background/Background'
+import Background from './components/Background/Background'
 export default function Home() {
   return (
     <>
